937c58ef13750f1b84d6776938fca90b99c75326
Jenny Parsons <jennylynparsons@gmail.com>
2023-06-18 13:09:30 -0600

    update branching statements and functionality of the submit button.

--- js/styles.js
+++ js/styles.js
@@ -1,12 +1,35 @@
 function handleSelect(event) {
   event.preventDefault();
-  const q1 = document.getElementById("#q1").value;
+  const q1 = parseInt(document.getElementById("q1").value);
+  const q2 = parseInt(document.getElementById("q2").value);
+  const q3 = parseInt(document.getElementById("q3").value);
+  const q4 = parseInt(document.getElementById("q4").value);
+  const q5 = parseInt(document.getElementById("q5").value);
 
-let results;
-  if (q1 === "App Design")
-    results = "Ruby";
+  let result;
 
-window.addEventListener("load", function() {
-  document.getElementById("form").addEventListener("submit", handleSelect);
-});
+  if (q1 + q2 + q3 + q4 + q5 === 5) {
+    result = "Ruby";
+  }
+  else if (q1 + q2 + q3 + q4 + q5 === 10) {
+    result = "C#";
+  }
+  else if (q1 + q2 + q3 + q4 + q5 === 15) {
+    result = "Python";
+  }
+  else if (q1 + q2 + q3 + q4 + q5 <= 15) {
+    result ="Python";
+  }
+
+
+
+  document.getElementById("result").innerText = result;
 }
+window.onload = function () {
+  const form = document.querySelector("form")
+  form.addEventListener("submit", handleSelect)
+}
+//window.addEventListener("load", function() {
+  //document.getElementById("form").addEventListener("submit", handleSelect);
+//});
+//}
