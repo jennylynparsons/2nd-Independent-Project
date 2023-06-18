3d1d54ffbf1b1f098c49e77e75baee20f2f9aba8
Jenny Parsons <jennylynparsons@gmail.com>
2023-06-17 21:04:09 -0600

    WIP, form not working.

--- js/styles.js
+++ js/styles.js
@@ -1,31 +1,12 @@
-function hideResults() {
-  document.getElementById("ruby").setAttribute("class", "hidden");
-  document.getElementById("c").setAttribute("class", "hidden");
-  document.getElementById("python").setAttribute("class", "hidden");
-}
-
 function handleSelect(event) {
   event.preventDefault();
-  const q1 = document.querySelector("#q1").value;
-  const q2 = document.querySelector("#q2").value;
+  const q1 = document.getElementById("#q1").value;
 
-  let result;
-  if (q2 === "Cat") {
-    result = "Ruby"
-}
-  //const q2 = document.getElementById("q2").value;
-  //const q3 = document.getElementById("q3").value;
-  //const q4 = document.getElementById("q4").value;
-  //const q5 = document.getElementById("q5").value;
-
+let results;
+  if (q1 === "App Design")
+    results = "Ruby";
 
 window.addEventListener("load", function() {
   document.getElementById("form").addEventListener("submit", handleSelect);
 });
-
-
-window.onload = function () {
-    const form = document.querySelector("form")
-    form.addEventListener("submit", handleSelect)
-  }
 }
